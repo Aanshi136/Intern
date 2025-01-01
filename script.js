d94884200585a79736f5d25b16ce52c1cf46172b
aanshi <aanshipatel136@gmail.com>
2025-01-01 14:45:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+    let count = 0;
+    function Count() {
+        for (let i = 0; i < 1; i++) {
+            count++;
+        }
+        document.getElementById("counterDisplay").innerHTML = "Count: " + count;
+    }
+
+
+   
