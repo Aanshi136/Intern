e3a91c64aae8d0cf43b91303a986445be1a4b640
aanshi <aanshipatel136@gmail.com>
2025-01-03 10:54:33 +0530

    To Do List

--- ToDoList.js
+++ ToDoList.js
@@ -1,12 +1,13 @@
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.querySelector(".myInput").value;  
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    var a = document.createTextNode(inputValue);
+    li.appendChild(a);
 
     if (inputValue === '') {
         alert("You must write something!");
     } else {
+        
         document.querySelector(".name").appendChild(li);  
     }
 
