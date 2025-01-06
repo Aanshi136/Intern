3289aafcbbadfd00bf95403f1a28cabe8dcd3197
aanshi <aanshipatel136@gmail.com>
2025-01-06 10:28:52 +0530

    ToDo list

--- ToDoList.js
+++ ToDoList.js
@@ -1,15 +1,55 @@
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.querySelector(".myInput").value;  
-    var a = document.createTextNode(inputValue);
-    li.appendChild(a);
+    let li = document.createElement("li");  
+    let inputValue = document.querySelector(".myInput").value;  
+    let errorMessage = document.querySelector(".error");  
+    let a = document.createTextNode(inputValue); 
+    li.appendChild(a);  
 
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
+
+
+    if (inputValue === '') {  
+        errorMessage.textContent = "You must write something!";
+    } else {  
+        errorMessage.textContent = "";
+
+
+
         
-        document.querySelector(".name").appendChild(li);  
+        let deleteB = document.createElement("button");
+        deleteB.innerHTML = '<i class="fa-solid fa-trash" style="color: #FFD43B;"></i>';  
+        deleteB.classList.add("deleteB");  
+        li.appendChild(deleteB);  
+        
+
+        
+
+
+        let updateB = document.createElement("button");
+        updateB.innerHTML = '<i class="fas fa-pencil-alt" style="color: #FFD43B;"></i>';  
+        updateB.classList.add("updateB");  
+        li.appendChild(updateB);  
+
+       
+
+
+        deleteB.onclick = function() {
+            li.remove();  
+        };
+
+        
+
+
+        updateB.onclick = function() {
+            var updatedText = prompt("Update task:", a.textContent);
+            if (updatedText !== null && updatedText !== '') {
+                a.textContent = updatedText;  
+            }
+        };
+
+        
+        document.querySelector(".name").appendChild(li);
     }
 
-    document.querySelector(".myInput").value = "";  
+    
+    document.querySelector(".myInput").value = "";
 }
